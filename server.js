1c316bbcee6229c41c1ccf8006956b9f87602b17
Raashul <rashul1996@gmail.com>
2016-10-28 18:41:38 -0400

    Changing procfile and server.js for heroku

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.post('/api/post/get', qaController.getPost);
 
 app.post('/api/answer/post', qaController.postAnswer)
 
-app.listen('3000', function(){
-	console.log('Listening in port 3000');
-})
+//app.listen('3000', function(){
+//	console.log('Listening in port 3000');
+//})
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
